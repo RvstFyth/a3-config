6f9461982ff42b3e255cb29cfb0a0775f39f0c99
RvStFyth <phpgeweld@gmail.com>
2020-03-09 13:58:02 +0100

    Added timeleft to MOCP script for polybar

--- polybar/.config/polybar/scripts/mocp.sh
+++ polybar/.config/polybar/scripts/mocp.sh
@@ -2,11 +2,13 @@
 
 if [ "$(mocp -Q %state)" != "STOP" ];then
     SONG=$(mocp -Q %song)
-        
+    TIMELEFT=$(mocp -Q %tl)
+
     if [ -n "$SONG" ]; then
         echo "$SONG - $(mocp -Q %album)"
     else
-        basename "$(mocp -Q %file)" .mp3
+        BASE=$(basename "$(mocp -Q %file)" .mp3)
+	echo "$TIMELEFT $BASE"
     fi
 else
     echo ""

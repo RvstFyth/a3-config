f448d1138b72b258a6fd87f1c89fa440bdc63860
RvStFyth <phpgeweld@gmail.com>
2020-06-16 20:57:05 +0200

    Qutebrowser config

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,28 @@
+config.source('qutewal.py')
+
+
+c.tabs.position = "left"
+c.tabs.width = "10%"
+c.colors.hints.fg = "#FFF"
+
+# password manager
+config.bind(',p', 'spawn --userscript qute-pass --dmenu-invocation dmenu')
+
+# Tabs and statusbar toggling
+config.bind('xx', 'config-cycle tabs.show always switching')
+config.bind('xz', 'config-cycle statusbar.hide')
+config.bind('xc', 'config-cycle statusbar.hide ;; config-cycle tabs.show always switching')
+
+
+c.url.searchengines = {
+   "DEFAULT":'https://duckduckgo.com/?q={}',
+   'ap': 'https://archlinux.org/packages/?q={}',
+   'aur': 'https://aur.archlinux.org/packages.php?K={}',
+   'ar': 'https://wiki.archlinux.org/?search={}',
+   "c": 'https://boards.4chan.org/{}/',
+   "r": 'https://www.reddit.com/r/{}',
+   "l": 'https://www.liveleak.com/browse?q={}',
+   "w": 'https://en.wikipedia.org/wiki/={}',
+   "y": 'https://www.youtube.com/results?search_query={}',
+   "g": "https://www.github.com/{}"
+}
